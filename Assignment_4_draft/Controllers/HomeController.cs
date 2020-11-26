eb2e5e3eedafbb8f2d3652243f0f595e242b001d
youzhou456 <52436690+youzhou456@users.noreply.github.com>
2020-11-26 18:22:56 -0500

    Add files via upload

---
+++ Assignment_4_draft/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Assignment_4_draft.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+
+namespace Assignment_4_draft.Controllers
+{
+  public class HomeController : Controller
+  {
+    HttpClient httpClient;
+
+    static string BASE_URL = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=E85";
+    static string API_KEY = "aPSzfmUw75qcrrEgVxsAS0bpJYLFIkB8snXYcUzR"; //Add your API key here inside ""
+
+    // Obtaining the API key is easy. The same key should be usable across the entire
+    // data.gov developer network, i.e. all data sources on data.gov.
+    // https://www.nps.gov/subjects/developer/get-started.htm
+
+    public IActionResult Index()
+    {
+      httpClient = new HttpClient();
+      httpClient.DefaultRequestHeaders.Accept.Clear();
+      httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+      httpClient.DefaultRequestHeaders.Accept.Add(
+          new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+      string NATIONAL_PARK_API_PATH = BASE_URL + "/stations?limit=10";
+      string parksData = "";
+
+      Stations record = null;
+
+      httpClient.BaseAddress = new Uri(NATIONAL_PARK_API_PATH);
+
+      try
+      {
+        HttpResponseMessage response = httpClient.GetAsync(NATIONAL_PARK_API_PATH).GetAwaiter().GetResult();
+
+        if (response.IsSuccessStatusCode)
+        {
+          parksData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+        }
+
+        if (!parksData.Equals(""))
+        {
+          // JsonConvert is part of the NewtonSoft.Json Nuget package
+          record = JsonConvert.DeserializeObject<Stations>(parksData);
+        }
+      }
+      catch (Exception e)
+      {
+        // This is a useful place to insert a breakpoint and observe the error message
+        Console.WriteLine(e.Message);
+      }
+
+      return View(record);
+    }
+  }
+}
---
+++ Assignment_4_draft/Models/Stations.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment_4_draft.Models
+{
+  public class Stations
+  {
+    public int total_result { get; set; }
+    public record[] data { get; set; }
+    public string station_locator_url { get; set; }
+  }
+
+  public class record
+  {
+    public string fuel_type_code { get; set; }
+    public string station_name { get; set; }
+    public string street_address { get; set; }
+    public string city { get; set; }
+    public string state { get; set; }
+    public string zip { get; set; }
+    public string country{ get; set; }
+    public string cards_accepted { get; set; }
+    public string access_days_time { get; set; }
+    public bool e85_blender_pump { get; set; }
+    public string lng_renewable_source { get; set; }
+    public string id { get; set; }
+ 
+  }
+}
